4438b6dd28d819aa398c5b529b4639d20de0cb40
Ramona Chantaf <ramonach.91@gmail.com>
2018-02-01 16:14:52 +0100

    skapat en popup

--- DocumentKeeper/src/controller/FolderContentPageController.java
+++ DocumentKeeper/src/controller/FolderContentPageController.java
@@ -6,15 +6,22 @@
 package controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * FXML Controller class
@@ -28,7 +35,7 @@ public class FolderContentPageController implements Initializable {
     
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {        
+    private void handleButtonAction(ActionEvent event) throws IOException {        
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Choose your files");
         List <File> selectedFiles = fileChooser.showOpenMultipleDialog(null);
@@ -36,6 +43,28 @@ public class FolderContentPageController implements Initializable {
         new ExtensionFilter("PDF Files", "*.pdf"));
         
         System.out.println(selectedFiles.get(0).getName());
+        addTag();
+    }
+    
+    @FXML
+    private void addTag() throws IOException { // läger till kunder i popUp1
+        
+        
+        Stage stage;
+        Parent root;
+        
+        stage = new Stage();
+        stage.setTitle("Lägg till namntagg");
+        stage.setResizable(false);
+        root = FXMLLoader.load(getClass().getResource("/fxml/TagPopUp.fxml"));
+        stage.setScene(new Scene(root));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(button.getScene().getWindow());
+        stage.setOnCloseRequest((WindowEvent we) -> {
+            stage.close();
+        });
+        stage.showAndWait(); // öppnar popUp
+ 
     }
     
     @Override
