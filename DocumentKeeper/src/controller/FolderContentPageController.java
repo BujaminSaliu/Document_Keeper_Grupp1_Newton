38be10fc18112daa889694a4bd6aa419d1097627
Dhiaa Eddin Anabtawi <dhiaaeddin.anabtawi@gmail.com>
2018-02-04 18:38:53 +0100

    Add encryption method. not called anywhere yet.

--- DocumentKeeper/src/controller/FolderContentPageController.java
+++ DocumentKeeper/src/controller/FolderContentPageController.java
@@ -6,9 +6,14 @@
 package controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.List;
@@ -26,6 +31,11 @@ import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import models.Document;
 import models.Tag;
 
@@ -125,6 +135,29 @@ public class FolderContentPageController implements Initializable {
 
     }
 
+    static void fileProcessor(int cipherMode,String key,File inputFile,File outputFile){
+	 try {
+	       Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+	       Cipher cipher = Cipher.getInstance("AES");
+	       cipher.init(cipherMode, secretKey);
+
+	       FileInputStream inputStream = new FileInputStream(inputFile);
+	       byte[] inputBytes = new byte[(int) inputFile.length()];
+	       inputStream.read(inputBytes);
+
+	       byte[] outputBytes = cipher.doFinal(inputBytes);
+
+	       FileOutputStream outputStream = new FileOutputStream(outputFile);
+	       outputStream.write(outputBytes);
+
+	       inputStream.close();
+	       outputStream.close();
+
+	    } catch (NoSuchPaddingException | NoSuchAlgorithmException 
+                     | InvalidKeyException | BadPaddingException
+	             | IllegalBlockSizeException | IOException e) {
+            }
+     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
