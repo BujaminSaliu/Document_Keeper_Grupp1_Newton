93142b8da1236a94a290f2e8c349dfe8c7b67904
Dhiaa Eddin Anabtawi <dhiaaeddin.anabtawi@gmail.com>
2018-02-07 11:13:05 +0100

    Change encryption so the files encrypted when they are saved, then decrypted when exporting them.

--- DocumentKeeper/src/controller/FolderContentPageController.java
+++ DocumentKeeper/src/controller/FolderContentPageController.java
@@ -12,7 +12,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.URL;
-import java.nio.file.Files;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
@@ -25,7 +24,6 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -99,7 +97,7 @@ public class FolderContentPageController implements Initializable {
     private String name = null;
     private String type = null;
     
-
+    String key = "This is a secret";
     
     private VBox selectedBox;
     
@@ -160,22 +158,16 @@ public class FolderContentPageController implements Initializable {
                     File source = new File(doc.getPath());
                     File dest = new File(dir + "/src/savedFiles/" + doc.getName() + "." + doc.getType());
 
-                    copyFileUsingJava7Files(source, dest);
                     oList.add(doc);
                     
                     //Here we encrypt original the file  
-                    String key = "This is a secret";
-                
                     //creating the encrypted file
-                    File encryptedFile = new File(doc.getPath() + ".encrypted");
+                    File encryptedFile = new File(dir + "/src/savedFiles/" + doc.getName()+ ".encrypted");
                 
                 try {
                     
                     //Call to encryption method file processor whitch uses "AES" algorithm
                     fileProcessor(Cipher.ENCRYPT_MODE,key,source,encryptedFile);
-                    
-                    //Delete the original file
-                    source.delete();
                     
                     //just to check if the encryption done!
                     System.out.println("Encrypted Successfully!");    
@@ -207,7 +199,7 @@ public class FolderContentPageController implements Initializable {
         try {
         final String dir = System.getProperty("user.dir");
         
-        File dest = new File(dir + "/src/savedFiles/" + name + "." + type);
+        File encryptedFile = new File(dir + "/src/savedFiles/" + name + ".encrypted");
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Export file");
         fileChooser.setInitialFileName(name + "." + type);
@@ -217,15 +209,15 @@ public class FolderContentPageController implements Initializable {
         fileChooser.getExtensionFilters().add(extFilter);
         
         File newFile = fileChooser.showSaveDialog(null);
-        
-        if(dest != null) {
-            copyFileUsingJava7Files(dest, newFile.getAbsoluteFile());
-            System.out.println(dest);
-        }
+
+        File decryptedFile = new File(newFile.getAbsolutePath());
+        System.out.println(newFile.getAbsolutePath());
+
+        fileProcessor(Cipher.DECRYPT_MODE, key, encryptedFile, decryptedFile);
+            
         } catch(Exception e){
             System.out.println(e);
         }
-
     }
     
     private void saveFileInfo(String fileName, String fileType){
@@ -392,8 +384,6 @@ public class FolderContentPageController implements Initializable {
             }
             columnCounter++;
         }
-        
-
     }
 
     @Override
@@ -418,10 +408,6 @@ public class FolderContentPageController implements Initializable {
         
     }
 
-    private void copyFileUsingJava7Files(File sourceFile, File destinationFile) throws IOException {
-        Files.copy(sourceFile.toPath(), destinationFile.toPath());
-    }
-
     static void fileProcessor(int cipherMode,String key,File inputFile,File outputFile) throws InvalidKeyException{
 	 try {
 	       Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
@@ -443,6 +429,7 @@ public class FolderContentPageController implements Initializable {
 	    } catch (NoSuchPaddingException | NoSuchAlgorithmException 
                      | BadPaddingException
 	             | IllegalBlockSizeException | IOException e) {
+                System.out.println(e.getMessage());
             }
      }
 }
